cc07ddd8fcb9b211c73e05bc505d0c4c1114f0f0
Tomas Mattia <tomasmattia@users.noreply.github.com>
2017-09-05 11:45:06 -0300

    Add files via upload

---
+++ MattiaAlbertiTomas - Sexta Clase/Ejercicio 01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades.Clase06;
+
+namespace Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[,] otroVec = new int[2, 3];  
+            //int[] vec = new int[3];
+            //otroVec[1,0] = 2;
+            //otroVec[0,1] = 3;
+            //otroVec[1,1] = 2;
+            //otroVec[0,2] = 3;
+            //otroVec[1,2] = 2;
+            //otroVec[0,0] = 3;
+            //foreach (int i in otroVec)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //double cant= 24;
+            //Tempera unaTempera = new Tempera(ConsoleColor.Cyan,"Maped",20);
+            //Tempera dosTempera = new Tempera(ConsoleColor.Cyan, "Maped", 52);
+            //Tempera tresTempera = new Tempera(ConsoleColor.Magenta, "Peped", 12);
+            //Console.WriteLine(unaTempera==tresTempera);
+            //Console.WriteLine(tresTempera+200);
+            //Console.WriteLine(Tempera.Mostrar(unaTempera));
+            Paleta unaPaleta = 0 ;
+            Console.WriteLine((string)unaPaleta);
+            Console.Read();
+        }
+    }
+}
---
+++ MattiaAlbertiTomas - Sexta Clase/Entidades.Clase06/Paleta.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Clase06
+{
+    public class Paleta
+    {
+        private Tempera[] _colores;
+        private int _cantMaximaColores;
+
+        private Paleta():this(5)
+        {   
+        }
+        private Paleta(int cantidad)
+        {
+            this._cantMaximaColores = cantidad;
+            this._colores = new Tempera[this._cantMaximaColores];
+            for (int i = 0; i < this._cantMaximaColores; i++)
+            {
+                this._colores[i] = new Tempera();
+            }
+        }
+
+        static public implicit operator Paleta(int cantidad)
+        {
+            return new Paleta(cantidad);
+        }
+
+        private string Mostrar()
+        {
+            string retorno = "Cantidad colores: " + this._cantMaximaColores;
+            retorno+="\nTemperas :\n";
+            foreach(Tempera temp in this._colores)
+            {
+                retorno+=Tempera.Mostrar(temp)+"\n";
+            }
+            return retorno;
+        }
+
+        static public explicit operator string(Paleta unaPaleta)
+        {
+            return unaPaleta.Mostrar();
+        }
+    }
+}
