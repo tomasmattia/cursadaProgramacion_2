86e99c4f452b929169d3e74b03ad1718432a427b
Tomas Mattia <tomasmattia@users.noreply.github.com>
2017-10-31 12:06:12 -0300

    Add files via upload

---
+++ Clase 17 - Mattia/TesteoConsola/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace TesteoConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProveedorDeDatos unProveedor = new ProveedorDeDatos();
+            foreach(Persona p in unProveedor.ObtenerPersonasBD())
+            {
+                Console.WriteLine(p.ToString());
+            }
+            Console.ReadKey();
+            Console.WriteLine("\n");
+            Console.WriteLine(unProveedor.ObtenerPersonaIdBD(2).ToString());
+            Console.ReadKey();
+            Console.WriteLine("\n");
+            try
+            {
+                Console.WriteLine(ProveedorDeDatos.ObtenerPersonaID(5).ToString());
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Se busco un id que no existe");
+            }
+            Console.ReadKey();
+            unProveedor.EliminarPersonaDB(new Persona(9, "a", "a", 25));
+            foreach (Persona p in unProveedor.ObtenerPersonasBD())
+            {
+                Console.WriteLine(p.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
